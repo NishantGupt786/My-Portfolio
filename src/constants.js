9d3decffd49b41b56eda77a0756e59761275d149
NishantGupta2325 <nishant.gupta@talentrecruit.com>
2024-08-04 22:27:40 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I mianly work with Next Js and Angular. I have also made many backend applications in Node js and Express. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/NishantGupt786" target="_blank">Github</a>!`,
+  "cs-degree": `This is a poster of my favourite sportsman Novak Djokovic. He recently established himself as the greatest tennis player of all time by securing the Olympic gold :).`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Anime and read Manga. 
+  I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the latest tech articles or exploring new software tools.`,
+  tv: `That's my TV. I love watching TV shows, movies, and anime. I just finished watching House MD, and it was really amazing. I also just watched Deadpool and Wolverine, which was probably the best movie I've seen in years.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="/nishantguptaupdatedfinal.pdf" target="_blank">Check it out?</a>
+  Contact me at nishantgupta2325@gmail.com if you have any interesting job opportunities!`,
+  projects: `I've had the opportunity to work on some exciting projects. I just finished a summer internship at TalentRecruit Pvt Ltd where I made significant contributions to both the Frontend and Backend of the application.
+  I also worked on a hybrid parser using flask, python. I'm currently architecting an ecommerce microservice for a client. I'm always looking for new projects to work on, so feel free to reach out to me!`,
+  library: `I mostly read manga and manwha, with a particular interest in the isekai genre. My shelves are filled with volumes of various series. I'm currently reading Doom Breaker, which has been incredibly engaging. Who else loves getting lost in alternate worlds and epic adventures?`,
+  exit: `If you want to exit Nishant's portfolio, just close the tab.`,
+};
