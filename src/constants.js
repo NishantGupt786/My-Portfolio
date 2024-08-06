e87e635bf5459d16a0fcbe08909b35a085c797b6
NishantGupt786 <nishantgupta2325@gmail.com>
2024-08-06 15:08:24 +0530

    feat:updated resume

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const dialogueData = {
   I also enjoy learning about new technologies and staying updated with the latest trends in the tech world. When I'm not coding, you can find me diving into the latest tech articles or exploring new software tools.`,
   tv: `That's my TV. I love watching TV shows, movies, and anime. I just finished watching House MD, and it was really amazing. I also just watched Deadpool and Wolverine, which was probably the best movie I've seen in years.`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="/nishantguptaupdatedfinal.pdf" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="/nishantguptaupdatedportfolio.pdf" target="_blank">Check it out?</a>
   Contact me at nishantgupta2325@gmail.com if you have any interesting job opportunities!`,
   projects: `I've had the opportunity to work on some exciting projects. I just finished a summer internship at TalentRecruit Pvt Ltd where I made significant contributions to both the Frontend and Backend of the application.
   I also worked on a hybrid parser using flask, python. I'm currently architecting an ecommerce microservice for a client. I'm always looking for new projects to work on, so feel free to reach out to me!`,
